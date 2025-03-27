3a0ab823c4172c558251f2f0965412893bd91819
Geoffrey Bunel <geoffreybunel1999@gmail.com>
2025-03-27 09:49:01 +0100

    Added the choice between words and sentences

--- scripts/script.js
+++ scripts/script.js
@@ -29,24 +29,41 @@ function lancerJeu() {
     // Initialisations
     let score = 0;
     let i = 0
+    let listeProposition = listeMots;
 
     let btnValiderMot = document.getElementById("btnValiderMot");
     let inputEcriture = document.getElementById("inputEcriture");
-    afficherProposition(listeMots[i]);
+    afficherProposition(listeProposition[i]);
     btnValiderMot.addEventListener("click", () => {
         console.log(inputEcriture.value);
-        if (inputEcriture.value === listeMots[i]) {
+        if (inputEcriture.value === listeProposition[i]) {
             score++;
         }
         i++;
         afficherResultat(score, i)
         inputEcriture.value = "";
-        if (listeMots[i] === undefined) {
+        if (listeProposition[i] === undefined) {
             afficherProposition("Le jeu est fini");
             btnValiderMot.disabled =  true;
         } else {
-            afficherProposition(listeMots[i]);
+            afficherProposition(listeProposition[i]);
         }
     })
+
+    let listBtnRadio = document.querySelectorAll(".optionSource input");
+    for (let index = 0 ; index < listBtnRadio.length; index++) {
+        listBtnRadio[index].addEventListener("change", (event) => {
+            console.log(event.target.value)
+            if (event.target.value === "1") {
+                listeProposition = listeMots;
+            } else {
+                listeProposition = listePhrases;
+            }
+            afficherProposition(listeProposition[i]);
+        })
+    }
+
+
+
     afficherResultat(score, i);
 }
